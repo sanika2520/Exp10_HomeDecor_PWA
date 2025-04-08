191fc23f681785e6136a44efb8fbec5a81c12edc
Sanika <sanikaambrale20@gmail.com>
2025-04-08 21:40:46 +0530

    Fix image paths

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-// // src/App.js
+
+
+
+
 // import React from "react";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import CartProvider from "./context/CartContext";
 // import HomePage from "./pages/HomePage";
 // import CartPage from "./pages/CartPage";
 // import Navbar from "./components/Navbar";
-// import "./styles/App.css";
+// import "./styles/styles.css";
 
 // function App() {
 //   return (
 //     <CartProvider>
 //       <Router>
 //         <Navbar />
-//         <main className="main-content">
-//           <Routes>
-//             <Route path="/" element={<HomePage />} />
-//             <Route path="/cart" element={<CartPage />} />
-//           </Routes>
-//         </main>
+//         <Routes>
+//           <Route path="/" element={<HomePage />} />
+//           <Route path="/cart" element={<CartPage />} />
+//         </Routes>
 //       </Router>
 //     </CartProvider>
 //   );
 // }
 
 // export default App;
-
 
 
 import React from "react";
@@ -38,7 +38,7 @@ import "./styles/styles.css";
 function App() {
   return (
     <CartProvider>
-      <Router>
+      <Router basename="/Exp10_HomeDecor_PWA">
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
--- src/data/products.js
+++ src/data/products.js
@@ -1,29 +1,123 @@
-// src/data/products.js
-const products = [
-    {
-      id: 1,
-      name: "Modern Sofa",
-      price: "$499",
-      image: "/images/image1.jpg", // Updated path
-    },
-    {
-      id: 2,
-      name: "Wooden Dining Table",
-      price: "$699",
-      image: "/images/image2.jpg", // You should add this image
-    },
-    {
-      id: 3,
-      name: "Luxury Bed",
-      price: "$999",
-      image: "/images/image3.jpg", // You should add this image
-    },
-    {
-      id: 4,
-      name: "Decorative Lamp",
-      price: "$79",
-      image: "/images/image4.jpg", // You should add this image
-    },
-  ];
+// // src/data/products.js
+// const products = [
+//     {
+//       id: 1,
+//       name: "Modern Sofa",
+//       price: "$499",
+//       image: "/images/image1.jpg", // Updated path
+//     },
+//     {
+//       id: 2,
+//       name: "Wooden Dining Table",
+//       price: "$699",
+//       image: "/images/image2.jpg", // You should add this image
+//     },
+//     {
+//       id: 3,
+//       name: "Luxury Bed",
+//       price: "$999",
+//       image: "/images/image3.jpg", // You should add this image
+//     },
+//     {
+//       id: 4,
+//       name: "Decorative Lamp",
+//       price: "$79",
+//       image: "/images/image4.jpg", // You should add this image
+//     },
+//   ];
   
-  export default products;
+//   export default products;
+
+
+
+// // src/data/products.js
+// const products = [
+//   {
+//     id: 1,
+//     name: "Modern Sofa",
+//     price: "$499",
+//     image: `${process.env.PUBLIC_URL}/images/image1.jpg`,
+//   },
+//   {
+//     id: 2,
+//     name: "Wooden Dining Table",
+//     price: "$699",
+//     image: `${process.env.PUBLIC_URL}/images/image2.jpg`,
+//   },
+//   {
+//     id: 3,
+//     name: "Luxury Bed",
+//     price: "$999",
+//     image: `${process.env.PUBLIC_URL}/images/image3.jpg`,
+//   },
+//   {
+//     id: 4,
+//     name: "Decorative Lamp",
+//     price: "$79",
+//     image: `${process.env.PUBLIC_URL}/images/image4.jpg`,
+//   },
+// ];
+
+// export default products;
+
+
+
+// const products = [
+//   {
+//     id: 1,
+//     name: "Modern Sofa",
+//     price: "$499",
+//     image: process.env.PUBLIC_URL + "/images/image1.jpg",
+//   },
+//   {
+//     id: 2,
+//     name: "Wooden Dining Table",
+//     price: "$699",
+//     image: process.env.PUBLIC_URL + "/images/image2.jpg",
+//   },
+//   {
+//     id: 3,
+//     name: "Luxury Bed",
+//     price: "$999",
+//     image: process.env.PUBLIC_URL + "/images/image3.jpg",
+//   },
+//   {
+//     id: 4,
+//     name: "Decorative Lamp",
+//     price: "$79",
+//     image: process.env.PUBLIC_URL + "/images/image4.jpg",
+//   },
+// ];
+
+// export default products;
+
+
+
+const products = [
+  {
+    id: 1,
+    name: "Wooden Dining Table",
+    price: "₹6,999",
+    image: process.env.PUBLIC_URL + "/images/image2.jpg",
+  },
+  {
+    id: 2,
+    name: "Modern Sofa Set",
+    price: "₹7,999",
+    image: process.env.PUBLIC_URL + "/images/image1.jpg",
+  },
+  {
+    id: 3,
+    name: "Luxury Bed",
+    price: "₹14,499",
+    image: process.env.PUBLIC_URL + "/images/image3.jpg",
+  },
+  {
+    id: 4,
+    name: "Decorative Lamp",
+    price: "₹3,799",
+    image: process.env.PUBLIC_URL + "/images/image4.webp",
+  },
+];
+
+export default products;
